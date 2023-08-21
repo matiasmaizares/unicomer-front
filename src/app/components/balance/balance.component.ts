8629566cb51c26b742ce75fd8ed930fbcf9a1071
matiasmaizares <matiasmaizares2017@gmail.com>
2023-08-21 02:22:37 -0300

    fin proyecto

--- src/app/components/balance/balance.component.ts
+++ src/app/components/balance/balance.component.ts
@@ -26,10 +26,7 @@ export class BalanceComponent {
   public barChartType: ChartType = 'bar';
   public barChartPlugins = [];
   public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40] },
-      { data: [28, 48, 40, 19, 86, 27, 90] },
-    ],
+    labels: ['2006', '2007', '2008', '2009'],
+    datasets: [{ data: [65, 59, 80, 81] }, { data: [28, 48, 40, 19] }],
   };
 }
